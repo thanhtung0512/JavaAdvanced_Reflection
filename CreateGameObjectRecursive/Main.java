a71a97149330d8e9f74847d829cbce559dba2529
Thanh Tung <21020399@vnu,edu.vn>
2022-12-08 18:06:55 +0700

    create game object recursively

---
+++ CreateGameObjectRecursive/Main.java
@@ -0,0 +1,69 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+class Character {
+    public Character() {
+        System.out.println("Instantiating Character");
+    }
+}
+
+class Background {
+    public Background() {
+        System.out.println("Instantiating background");
+    }
+}
+
+class Enemy {
+    public Enemy() {
+        System.out.println("Instantiating enemy");
+    }
+}
+
+class Game {
+    private Character character;
+    private Background background;
+    private Enemy enemy;
+
+    public Game(Character character, Background background, Enemy enemy) {
+        this.character = character;
+        this.background = background;
+        this.enemy = enemy;
+    }
+
+    public void run() {
+        System.out.println("Running");
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args)
+            throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        Game game = createObjectRecursively(Game.class);
+        game.run();
+    }
+
+    public static <T> T createObjectRecursively(Class<T> clazz)
+            throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        Constructor<?> firstConstructor = getFirstConstructor(clazz);
+        List<Object> constructorArguments = new ArrayList<>();
+
+        for (Class<?> eachClass : firstConstructor.getParameterTypes()) {
+            constructorArguments.add(createObjectRecursively(eachClass));
+        }
+
+        return (T) firstConstructor.newInstance(constructorArguments.toArray());
+    }
+
+    private static Constructor<?> getFirstConstructor(Class<?> clazz) {
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+        if (constructors.length == 0) {
+            throw new IllegalStateException(
+                    String.format("No constructor has been found for class %s ", clazz.getSimpleName()));
+        }
+
+        return constructors[0];
+    }
+}
--- Web/Main.java
+++ Web/Main.java
@@ -1,21 +1,28 @@
 import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 
 import WebSeverr.WebSever;
 import init.ServerConfiguration;
 
 public class Main {
-    public static void main(String[] args) throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, IOException {
+    public static void main(String[] args) throws NoSuchMethodException, SecurityException, InstantiationException,
+            IllegalAccessException, IllegalArgumentException, InvocationTargetException, IOException {
         initServerConfiguration();
         WebSever webSever = new WebSever();
         webSever.startSever();
     }
 
-    public static void initServerConfiguration() throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, IOException {
-        Constructor<?> constructor = ServerConfiguration.class.getDeclaredConstructor(int.class,String.class);
+    public static void initServerConfiguration()
+            throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException,
+            IllegalArgumentException, InvocationTargetException, IOException {
+        Constructor<?> constructor = ServerConfiguration.class.getDeclaredConstructor(int.class, String.class);
         constructor.setAccessible(true);
-        constructor.newInstance(8080,"Good day!");
-       
+        constructor.newInstance(8080, "Good day!");
+
     }
+
+    
 }
